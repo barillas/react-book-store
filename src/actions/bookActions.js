1e72a0436fd8a98ad1f8f97521e25cfbc4ac3ba5
Oscar Barillas <oscarbarillas93@gmail.com>
2017-03-04 18:11:46 -0800

    Form working with React + Redux

--- src/actions/bookActions.js
+++ src/actions/bookActions.js
@@ -1,10 +1,52 @@
 // ./src/actions/bookActions.js
-export const createBook = (book) => {
-  // Return action
+// API URL
+
+import Axios from 'axios';
+
+const apiUrl = 'http://58bb6f69673bee120092eea5.mockapi.io/books';
+// Sync Action
+export const fetchBooksSuccess = (books) => {
   return {
-    // Unique identifier
-    type: 'CREATE_BOOK',
-    // Payload
-    book: book
+    type: 'FETCH_BOOKS_SUCCESS',
+    books
+  }
+};
+//Async Action
+export const fetchBooks = () => {
+  // Returns a dispatcher function
+  // that dispatches an action at a later time
+  return (dispatch) => {
+    // Returns a promise
+    return Axios.get(apiUrl)
+      .then(response => {
+        // Dispatch another action
+        // to consume data
+        dispatch(fetchBooksSuccess(response.data))
+      })
+      .catch(error => {
+        throw(error);
+      });
   };
+};
+
+export const createBook = (book) => {
+  return (dispatch) => {
+    return Axios.post(apiUrl, book)
+      .then(response => {
+        // Dispatch a synchronous action
+        // to handle data
+        dispatch(createBookSuccess(response.data))
+      })
+      .catch(error => {
+        throw(error);
+      });
+  };
+};
+
+// ./src/actions/bookActions.js
+export const createBookSuccess = (book) => {
+  return {
+    type: 'CREATE_BOOK_SUCCESS',
+    book
+  }
 };
--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import * as bookActions from './actions/bookActions';
 
 import configureStore from './store/configureStore';
 
 const store = configureStore();
+store.dispatch(bookActions.fetchBooks());
+console.log(store.dispatch(bookActions.fetchBooks()));
 
 render(
   <Provider store={store}>
